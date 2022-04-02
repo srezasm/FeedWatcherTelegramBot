c6fb90fb56c3e1f99c71549d33814ba0fa47f9bf
srezasm <srezas.2017@gmail.com>
2022-04-02 08:07:03 +0430

    config module modified
    some new functions and impovements

--- src/config.py
+++ src/config.py
@@ -22,7 +22,11 @@ def getchannels():
 
 def getchannelfeeds(channelid):
     with open('config.json') as fr:
-        return json.load(fr)['channels'][channelid]['feeds']
+        return list(json.load(fr)['channels'][channelid]['feeds'])
+
+def getfeedinfo(channelid, feedname):
+    with open('config.json') as fr:
+        return json.load(fr)['channels'][channelid]['feeds'][feedname]
 
 def getfeedcategory(channelid, feedname):
     with open('config.json') as fr:
@@ -31,3 +35,19 @@ def getfeedcategory(channelid, feedname):
 def getformatbycategory(channelid, category):
     with open('config.json') as fr:
         return json.load(fr)['channels'][channelid]['formats'][category]
+
+def getids():
+    with open('config.json') as fr:
+        return json.load(fr)['idsfile']
+
+def getetags():
+    with open('config.json') as fr:
+        return json.load(fr)['etagsfile']
+
+def writeids(newids: str):
+    with open(getids(), 'w') as fw:
+            fw.write(newids)
+
+def writeetags(newetag: str):
+    with open(getids(), 'w') as fw:
+            fw.write(newetag)
