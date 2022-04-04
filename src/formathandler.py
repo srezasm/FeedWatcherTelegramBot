88278b00294cfdf79ca30fd70c0f4c7909c98382
srezasm <srezas.2017@gmail.com>
2022-04-04 21:49:36 +0430

    format_date() done

--- src/formathandler.py
+++ src/formathandler.py
@@ -1,7 +1,7 @@
-from ast import arg
-from os import link
-from pydoc import describe
+from time import gmtime
+import pytz
 from bs4 import BeautifulSoup
+from dateutil import parser
 import utils
 import config
 import regex
@@ -25,26 +25,10 @@ def format_item(item, feedinfo, channelid):
                                 body=desc, url=link, hashtags=hashtags, pubdate=pubdate)
     return posttext
 
-# Tue, 21 Dec 2021 10:00:00 -0400
-# Thu, 01 Nov 2018 00:00:00 +0000
-# Fri, 12 Nov 2021 21:52:35 GMT
-# 2021-10-31T00:00:00+00:00
-# 2022-03-17T17:27:16+03:30
-# 2020-07-27T14:56:50+00:00
 def format_date(date: str):
-    if regex.match('^{3}, \d\d \w{3} \d{4} \d\d:\d\d:\d\d', date):
-        region = regex.search('([A-Z]{3})?(\+\d{4})?(\-\d{4})?$', date).group()
-        if regex.match('\w{3}', region):
-			# log info the match
-            region = "+0000"
-
-        date = date.replace(region, '').rstrip()
-
-    if regex.match('', date):
-        return
-
-    #! log [date] and details if not found
-
+    parsed = parser.parse(date)
+    gmt = str(pytz.timezone('GMT').normalize(parsed))
+    return gmt.replace('+00:00', ' GMT')
 
 def format_description(description):
     return
